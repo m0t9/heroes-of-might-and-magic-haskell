424af2b1eac235628b043e688103ca7f69b59a99
Dan <dan@dminc.ru>
2023-07-08 17:08:30 +0300

    feat: add test field

--- game/Main.hs
+++ game/Main.hs
@@ -1,4 +1,10 @@
 module Main where
-
+import Graphics
+import Graphics.Gloss
+window :: Display
+window = InWindow "Nice Window" (200, 200) (10, 10)
+background :: Color
+background = white
+sizeOfField = (3, 4)
 main :: IO ()
-main = print "Hello world!"
+main = display window background (drawField (0, 0) sizeOfField)
