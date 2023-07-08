4774750ce02253edabb1b2743d2cd4e598fe87b9
Dan <dan@dminc.ru>
2023-07-08 17:59:25 +0300

    chore: update test picture

--- game/Main.hs
+++ game/Main.hs
@@ -2,9 +2,9 @@ module Main where
 import Graphics
 import Graphics.Gloss
 window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
+window = InWindow "/DaniilNikulin" (200, 200) (10, 10)
 background :: Color
 background = white
 sizeOfField = (3, 4)
 main :: IO ()
-main = display window background (drawField (0, 0) sizeOfField)
+main = display window background (drawField (0, 0) sizeOfField) --(polygon [(0, 0), (0, 1), (1, 1), (1, 0)])--
