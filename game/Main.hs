d18a7ab8de4b48d3024e20595213f288dba6abc5
Dan <dan@dminc.ru>
2023-07-09 16:14:48 +0300

    feat: add test simulation

--- game/Main.hs
+++ game/Main.hs
@@ -1,10 +1,23 @@
 module Main where
 import Graphics
 import Graphics.Gloss
+import GameInternal
+import GameHandler
 window :: Display
-window = InWindow "/DaniilNikulin" (200, 200) (10, 10)
+window = InWindow "/DaniilNikulin" (1200, 800) (100, 100)
 background :: Color
 background = white
-sizeOfField = (15, 11)
+world :: State
+world = NoSelected (GameState units player)
 main :: IO ()
-main = display window background (drawField (0, 0) sizeOfField) --(polygon [(0, 0), (0, 1), (1, 1), (1, 0)])--
+--main = display window background (drawField (0, 0) units) --(polygon [(0, 0), (0, 1), (1, 1), (1, 0)])--
+main = play window background 1 world renderState gameHandler timeHandler
+
+units :: [Unit]
+units = [Unit Archer (UnitState (getInitialProps Archer) player (1, 1) 0 0 0), Unit Archer (UnitState (getInitialProps Archer) player (0, 0) 0 0 0), Unit Archer (UnitState (getInitialProps Archer) player (1, 0) 0 0 0), Unit Archer (UnitState (getInitialProps Archer) player (0, 1) 0 0 0)]
+
+player :: Player
+player = Player True
+
+timeHandler :: (Float -> world -> world)
+timeHandler _dt wrld = wrld 
