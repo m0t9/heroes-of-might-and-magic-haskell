4e50eace8b15a2e8aedd4e096879dfb2e23838fd
SadPixels <a.tomashov@innopolis.university>
2023-07-09 04:34:43 +0300

    Fixed field size

--- game/Main.hs
+++ game/Main.hs
@@ -5,6 +5,6 @@ window :: Display
 window = InWindow "/DaniilNikulin" (200, 200) (10, 10)
 background :: Color
 background = white
-sizeOfField = (11, 15)
+sizeOfField = (15, 11)
 main :: IO ()
 main = display window background (drawField (0, 0) sizeOfField) --(polygon [(0, 0), (0, 1), (1, 1), (1, 0)])--
