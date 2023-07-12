4d4739859738d579e132f8a398fbab2ea544c2af
Aleksandr Tomashov <a.tomashov@innopolis.university>
2023-07-12 03:20:51 +0300

    replace function

--- game/Utils.hs
+++ game/Utils.hs
@@ -28,6 +28,12 @@ getPath (x1, y1) (x2, y2) =
       | x2' > x1'  = [x1'..x2']
       | otherwise  = reverse [x2'..x1']
 
+replace :: Eq a => a -> a -> [a] -> [a]
+replace _ _ [] = []
+replace xOld xNew (x:xs) 
+  | xOld == x = xNew : xs
+  | otherwise = x : replace xOld xNew xs  
+
 sign :: Int -> Int
 sign num
   | num > 0   = 1
