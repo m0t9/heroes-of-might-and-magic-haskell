e41c3f412112ea094df176cbced084d696319b5c
LeonidM <wildhs.botm@gmail.com>
2023-07-13 19:21:22 +0300

    Changed units

--- game/Main.hs
+++ game/Main.hs
@@ -57,29 +57,30 @@ main = do
 
 
 units :: [Unit]
+units :: [Unit]
 units = [
-    createUnit Pikeman player1 (0, 0) 100,
-    createUnit Archer player1 (0, 1) 100,
-    createUnit Swordsman player1 (0, 2) 100,
-    createUnit Monk player1 (0, 3) 100,
-    createUnit Dwarf player1 (0, 4) 100,
-    createUnit WoodElf player1 (0, 5) 100,
-    createUnit DenroidGuard player1 (0, 6) 100,
-    createUnit Troglodyte player1 (0, 7) 100,
-    createUnit Harpy player1 (0, 8) 100,
-    createUnit Beholder player1 (0, 9) 100,
-    createUnit Minotaur player1 (0, 10) 100,
+    createUnit Pikeman player1 (0, 0) 80,
+    createUnit Archer player1 (0, 1) 10,
+    createUnit Swordsman player1 (0, 2) 15,
+    createUnit Monk player1 (0, 3) 10,
+    createUnit Dwarf player1 (0, 4) 33,
+    createUnit WoodElf player1 (0, 5) 25,
+    createUnit DenroidGuard player1 (0, 6) 23,
+    createUnit Troglodyte player1 (0, 7) 1,
+    createUnit Harpy player1 (0, 8) 20,
+    createUnit Beholder player1 (0, 9) 29,
+    createUnit Minotaur player1 (0, 10) 10,
 
-    createUnit Pikeman player2 (14, 0) 1,
+    createUnit Pikeman player2 (14, 0) 80,
     createUnit Archer player2 (14, 1) 10,
-    createUnit Swordsman player2 (14, 2) 1,
+    createUnit Swordsman player2 (14, 2) 15,
     createUnit Monk player2 (14, 3) 10,
-    createUnit Dwarf player2 (14, 4) 10,
-    createUnit WoodElf player2 (14, 5) 10,
-    createUnit DenroidGuard player2 (14, 6) 10,
+    createUnit Dwarf player2 (14, 4) 33,
+    createUnit WoodElf player2 (14, 5) 25,
+    createUnit DenroidGuard player2 (14, 6) 23,
     createUnit Troglodyte player2 (14, 7) 1,
-    createUnit Harpy player2 (14, 8) 1,
-    createUnit Beholder player2 (14, 9) 10,
+    createUnit Harpy player2 (14, 8) 20,
+    createUnit Beholder player2 (14, 9) 29,
     createUnit Minotaur player2 (14, 10) 10
   ]
 
